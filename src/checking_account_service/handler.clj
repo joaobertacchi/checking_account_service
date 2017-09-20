bb65e23783db701affff20d5cc0400cca43d1886
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-09-20 19:49:08 -0300

    Configure example data for swagger

--- src/checking_account_service/handler.clj
+++ src/checking_account_service/handler.clj
@@ -5,6 +5,7 @@
             [clj-time.core :as t]
             [clj-time.format :as f]
             [cheshire.generate :as generate]
+            [ring.swagger.json-schema :as rjs]
             [checking_account_service.models.operation :as Operation]
             [checking_account_service.models.account :as Account]
             [checking_account_service.models.statement :as Statement]
@@ -122,27 +123,70 @@
       (context "/accounts" []
 
         (context "/:account_number" []
-          :path-params [account_number :- AccountNumber]
+          :path-params [account_number :- (rjs/field AccountNumber {:default 1})]
 
           (GET "/balance" []
-            :return Balance
+            :return (rjs/field Balance {:example
+              {
+                :account_number 1
+                :balance 153.3
+              }})
             :summary "Get the current balance for the given checking account"
             (get-balance-handler account_number))
 
           (POST "/operations" []
-            :return OperationOut
-            :body [operation OperationIn]
+            :return (rjs/field OperationOut {:example
+              {
+                :id 3637
+                :account_number 1
+                :description "Purchase on Amazon"
+                :amount 100.2
+                :date (t/today)
+              }})
+            :body [operation (rjs/field OperationIn {:example
+              {
+                :description "Purchase on Amazon"
+                :amount 100.2
+                :date (t/today)
+              }})]
             :summary "Add an operation to a given checking account"
             (create-operation-handler account_number operation))
             
           (GET "/statement" []
-            :return Statement
+            :return (rjs/field Statement {:example
+              {
+                :account_number 1,
+                :start_date (t/today)
+                :end_date (t/plus (t/today) (t/days 7))
+                :day_statements [
+                  {
+                    :date (t/today)
+                    :operations [
+                      {
+                        :description "Purchase on Amazon"
+                        :amount 100.2
+                      }
+                    ]
+                    :balance 100.2
+                  }
+                ]
+              }})
             :query-params [start_date :- Date, end_date :- Date]
             :summary "Returns the bank statement of an account given a period of dates"
             (get-statement-handler account_number start_date end_date))
             
           (GET "/debts" []
-            :return Debts
+            :return (rjs/field Debts {:example
+              {
+                :account_number 1
+                :debts [
+                  {
+                    :principal -27.35
+                    :start (t/today)
+                    :end (t/plus (t/today) (t/days 2))
+                  }
+                ]
+              }})
             :summary "Returns the periods which the account's balance was negative"
             (get-debts-handler account_number))
 
