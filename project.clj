e3acd7196087a3dc8fa0aba3b37508010788104c
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-09-20 23:57:15 -0300

    Use better tests support

--- project.clj
+++ project.clj
@@ -7,5 +7,7 @@
    :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [cheshire "5.5.0"]
                                   [ring/ring-mock "0.3.0"]
-                                  [clj-time "0.14.0"]]
+                                  [clj-time "0.14.0"]
+                                  [pjstadig/humane-test-output "0.8.3"]
+                                  [com.jakemccrary/lein-test-refresh "0.21.1"]]
                    :plugins [[lein-ring "0.12.0"]]}})
